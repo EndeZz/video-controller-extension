de417fe92924737fdd0a999a4930cfaa646732a7
EndeZz <r-serega13@yandex.ru>
2024-01-01 00:33:03 +0400

    feature: add locales

--- src/shared/configs/manifest.config.ts
+++ src/shared/configs/manifest.config.ts
@@ -1,13 +1,17 @@
 import { ManifestV3Export, defineManifest } from '@crxjs/vite-plugin'
 import pkg from '../../../package.json'
 
-const { name, description, version } = pkg
+const { version } = pkg
+
+const name = '__MSG_name__'
+const description = '__MSG_description__'
 
 export const manifest: ManifestV3Export = defineManifest(async (env) => ({
   manifest_version: 3,
-  name: env.mode === 'development' ? `${name}-dev` : name,
+  name: env.mode === 'development' ? `[DEV] ${name}` : name,
   description,
   version,
+  default_locale: 'en',
   icons: {
     '16': 'public/icon16x16.png',
     '32': 'public/icon48x48.png',
