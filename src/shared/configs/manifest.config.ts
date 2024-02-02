a77fb198937705606c8dca84f48fb1000e7d2dc0
EndeZz <r-serega13@yandex.ru>
2024-02-02 12:00:19 +0400

    feature: add content in manifest config

--- src/shared/configs/manifest.config.ts
+++ src/shared/configs/manifest.config.ts
@@ -1,15 +1,15 @@
 import { ManifestV3Export, defineManifest } from '@crxjs/vite-plugin'
 import pkg from '../../../package.json'
+import { getTranslationContent } from '../lib/utils'
 
 const { version } = pkg
 
-const name = '__MSG_name__'
-const description = '__MSG_description__'
+const content = getTranslationContent()
 
 export const manifest: ManifestV3Export = defineManifest(async (env) => ({
   manifest_version: 3,
-  name: env.mode === 'development' ? `[DEV] ${name}` : name,
-  description,
+  name: env.mode === 'development' ? `[DEV] ${content.name}` : content.name,
+  description: content.description,
   version,
   default_locale: 'en',
   icons: {
@@ -21,4 +21,19 @@ export const manifest: ManifestV3Export = defineManifest(async (env) => ({
   action: {
     default_popup: 'src/modules/popup/popup.html',
   },
+  content_scripts: [
+    {
+      matches: ['<all_urls>'],
+      js: ['src/modules/content-script/content-script.ts'],
+      run_at: 'document_idle',
+      all_frames: false,
+    },
+  ],
+  host_permissions: ['<all_urls>'],
+  web_accessible_resources: [
+    {
+      matches: ['<all_urls>'],
+      resources: ['src/modules/content-script/content-script.css'],
+    },
+  ],
 }))
